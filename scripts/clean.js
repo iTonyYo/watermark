7ca84e3d8b29f24b159211d2ca3930a4ece16358
Tony Stark <swzyocowboy@icloud.com>
2019-01-04 11:01:57 +0800

    修复: :bug: `execa()` 是异步函数，但未通过 `await` 等待 `execa()` 的完成

--- scripts/clean.js
+++ scripts/clean.js
@@ -3,10 +3,12 @@ const execa = require('execa');
 const { resolveApp } = require('./paths');
 const scriptsDir = resolveApp('scripts');
 
-execa('node', [
-  `${scriptsDir}/clean-reports.js`
-]);
+(async () => {
+  await execa('node', [
+    `${scriptsDir}/clean-reports.js`
+  ]);
 
-execa('node', [
-  `${scriptsDir}/clean-cache.js`
-]);
+  await execa('node', [
+    `${scriptsDir}/clean-cache.js`
+  ]);
+})();
